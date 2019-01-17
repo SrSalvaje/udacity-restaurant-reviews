31f244cc23bce1f5e688a361bb126cb7499340d8
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-01-17 13:20:46 +0100

    feat: added mapbox api key

--- app/js/main.js
+++ app/js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoiam9ubWlyZW4iLCJhIjoiY2pyMGt0b3FmMG1hNTN4cGNzbDV4YzQ0NSJ9.BxlmxnUMp-cylQQ5KQbl-Q',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
