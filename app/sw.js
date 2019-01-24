d2d1d8cc7a0e95398c7b21d8359f03dfee330dd2
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-01-24 13:20:17 +0100

    feat: cached static files

--- app/sw.js
+++ app/sw.js
@@ -1,1 +1,42 @@
+const appName="Udacity-Restaurant-Reviews",
+    staticCacheName=`${appName}-v1.0`,
+    imgCache = `${appName}-images`;
+let allCaches=[
+        staticCacheName,
+        imgCache
+    ],
+    filesToCache=[
+        "/",
+        "/index.html",
+        "/restaurant.html",
+        "/css/styles.css",
+        "/css/responsive.css",
+        "/js/dbhelper.js",
+        "/js/main.js",
+        "/js/restaurant_info.js",
+        "/data/restaurants.json"
+    ];
 
+//cache assets on installation
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(staticCacheName).then(function(cache){
+            return cache.addAll(filesToCache);
+        })
+    ); 
+});
+
+//delete previous caches (based on version)//
+/* self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName){
+                    return cacheName.startsWith(appName) && !allCaches.includes(cacheName);
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })   
+            );
+        })
+    );
+}); */ 
